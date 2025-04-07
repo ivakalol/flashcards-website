5f8cbfaf816f33eee50803cc1a4f1575287eda53
Ivaylo Chernev <ivko0527@gmail.com>
2025-04-08 01:16:03 +0300

    Removed Unused old files

--- assets/js/DataBase.js
+++
@@ -1,4 +0,0 @@
-// This file will be used for database operations in the future.
-// For now, localStorage is used in the main.js file.
-
-console.log('Database module loaded');
--- assets/js/navigation.js
+++
@@ -1,4 +0,0 @@
-// This file could be used for advanced navigation features in the future.
-// For now, navigation is handled in the main.js file.
-
-console.log('Navigation module loaded');
